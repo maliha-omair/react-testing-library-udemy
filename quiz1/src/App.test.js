2da9e21a855b56c0384bf83c97b49b5ea78a204c
Maliha Omair <maliha@malihaomair.com>
2023-01-11 21:54:50 -0500

    added prettier

--- quiz1/src/App.test.js
+++ quiz1/src/App.test.js
@@ -1,26 +1,25 @@
-import { render, screen, fireEvent} from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('check button is enabled and checbox is unchecked', () => {
+test("check button is enabled and checbox is unchecked", () => {
   render(<App />);
   //button is enabled
-  const buttonElement = screen.getByRole('button',{ name: 'button'});
+  const buttonElement = screen.getByRole("button", { name: "button" });
   expect(buttonElement).toBeEnabled();
 
   //checkbox is unchecked
-  const checkbox = screen.getByRole('checkbox');
+  const checkbox = screen.getByRole("checkbox");
   expect(checkbox).not.toBeChecked();
 });
 
-test('check button is disabled on checkbox checked', () => {
+test("check button is disabled on checkbox checked", () => {
   render(<App />);
-  const buttonElement = screen.getByRole('button',{ name: 'button'});
-  const checkbox = screen.getByRole('checkbox');
+  const buttonElement = screen.getByRole("button", { name: "button" });
+  const checkbox = screen.getByRole("checkbox");
 
   fireEvent.click(checkbox);
   expect(buttonElement).toBeDisabled();
 
   fireEvent.click(checkbox);
   expect(buttonElement).toBeEnabled();
-
 });
