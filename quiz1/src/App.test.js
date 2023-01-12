6ba6d9fb776af88798a8b0cb878bb4d143999d47
Maliha Omair <maliha@malihaomair.com>
2023-01-11 21:01:48 -0500

    added estlint

--- quiz1/src/App.test.js
+++ quiz1/src/App.test.js
@@ -6,19 +6,17 @@ test('check button is enabled and checbox is unchecked', () => {
   //button is enabled
   const buttonElement = screen.getByRole('button',{ name: 'button'});
   expect(buttonElement).toBeEnabled();
-  
+
   //checkbox is unchecked
   const checkbox = screen.getByRole('checkbox');
   expect(checkbox).not.toBeChecked();
-
-  
 });
 
 test('check button is disabled on checkbox checked', () => {
   render(<App />);
   const buttonElement = screen.getByRole('button',{ name: 'button'});
   const checkbox = screen.getByRole('checkbox');
- 
+
   fireEvent.click(checkbox);
   expect(buttonElement).toBeDisabled();
 
